27e89a5bb2e7d60e9be2ccfc8dabcf2d32add5e7
Luca Mussi <ing.luca.mussi@gmail.com>
2015-02-03 14:07:32 +0100

    better indentation

--- server/atmosphere.js
+++ server/atmosphere.js
@@ -1,74 +1,74 @@
 Frontends = new Meteor.Collection("frontends");
 
 var frontends = [
-	{authorName: "useraccounts", baseName: "bootstrap"  , fUrl: "http://getbootstrap.com/"},
-	{authorName: "useraccounts", baseName: "foundation" , fUrl: "http://foundation.zurb.com/"},
-	{authorName: "useraccounts", baseName: "ionic"      , fUrl: "http://ionicframework.com/"},
-	{authorName: "useraccounts", baseName: "polymer"    , fUrl: "https://www.polymer-project.org/"},
-	{authorName: "useraccounts", baseName: "semantic-ui", fUrl: "http://semantic-ui.com/"},
-	{authorName: "useraccounts", baseName: "ratchet"    , fUrl: "http://goratchet.com/"},
-	{authorName: "useraccounts", baseName: "unstyled"   , fUrl: ""},
+  {authorName: "useraccounts", baseName: "bootstrap"  , fUrl: "http://getbootstrap.com/"},
+  {authorName: "useraccounts", baseName: "foundation" , fUrl: "http://foundation.zurb.com/"},
+  {authorName: "useraccounts", baseName: "ionic"      , fUrl: "http://ionicframework.com/"},
+  {authorName: "useraccounts", baseName: "polymer"    , fUrl: "https://www.polymer-project.org/"},
+  {authorName: "useraccounts", baseName: "semantic-ui", fUrl: "http://semantic-ui.com/"},
+  {authorName: "useraccounts", baseName: "ratchet"    , fUrl: "http://goratchet.com/"},
+  {authorName: "useraccounts", baseName: "unstyled"   , fUrl: ""},
 ];
 
 _.each(frontends, function(frontend){
-	var f = Frontends.findOne(frontend);
-	if (!f) {
-		Frontends.insert(frontend);
-	}
+  var f = Frontends.findOne(frontend);
+  if (!f) {
+    Frontends.insert(frontend);
+  }
 });
 
 /*
 Sample object got from the response
 {
-	"installs-per-year": 385,
-	"latestVersion": {
-		"published": {
-			"$date": 1422485989711
-		},
-		"version": "1.6.1",
-		"git": "https://github.com/meteor-useraccounts/core.git",
-		"description": "Meteor sign up and sign in templates core package.",
-		"readme": "https://warehouse.meteor.com/readme/u8BPj5eWZzHQiFM6q/1422485987517/AnhA9SZLf3/useraccounts:core-1.6.1-readme.md",
-		"unmigrated": false
-	},
-	"name": "useraccounts:core",
-	"score": 1.7110424750212552,
-	"starCount": 35
+  "installs-per-year": 385,
+  "latestVersion": {
+    "published": {
+      "$date": 1422485989711
+    },
+    "version": "1.6.1",
+    "git": "https://github.com/meteor-useraccounts/core.git",
+    "description": "Meteor sign up and sign in templates core package.",
+    "readme": "https://warehouse.meteor.com/readme/u8BPj5eWZzHQiFM6q/1422485987517/AnhA9SZLf3/useraccounts:core-1.6.1-readme.md",
+    "unmigrated": false
+  },
+  "name": "useraccounts:core",
+  "score": 1.7110424750212552,
+  "starCount": 35
 }
 */
 
 Meteor.setTimeout(Meteor.http.get(
-	"https://atmospherejs.com/a/packages",
-	{
-		headers: {'Accept': 'application/json'}
-	},
-	function(error, response){
-		console.log("Getting packages data from atmosphere...");
-		// Goes through each package
-		if (!error) {
-			_.each(response.data, function(pkg){
-				var
-				  pkgName = pkg.name.split(":"),
-					authorName = pkgName[0],
-					baseName = pkgName[1]
-				;
-				if (authorName === "useraccounts") {
-					var frontend = Frontends.findOne({baseName: baseName});
-					if (frontend) {
-						console.dir(pkg);
-						var newFields = _.pick(pkg, 'name', 'starCount');
-						if (pkg.latestVersion && pkg.latestVersion.version) {
-							newFields.version = pkg.latestVersion.version;
-						}
-						if (pkg["installs-per-year"]) {
-							newFields.count = pkg["installs-per-year"];
-						}
-						Frontends.update(frontend._id, { $set: newFields });
-					}
-				}
-			});
-		}
-	}
+  "https://atmospherejs.com/a/packages",
+  {
+    headers: {'Accept': 'application/json'}
+  },
+  function(error, response){
+    console.log("Getting packages data from atmosphere...");
+    // Goes through each package
+    if (!error) {
+      _.each(response.data, function(pkg){
+        var
+          pkgName = pkg.name.split(":"),
+          authorName = pkgName[0],
+          baseName = pkgName[1]
+        ;
+        if (authorName === "useraccounts") {
+          var frontend = Frontends.findOne({baseName: baseName});
+          if (frontend) {
+            console.dir(pkg);
+            var newFields = _.pick(pkg, 'name', 'starCount');
+            if (pkg.latestVersion && pkg.latestVersion.version) {
+              newFields.version = pkg.latestVersion.version;
+            }
+            if (pkg["installs-per-year"]) {
+              newFields.count = pkg["installs-per-year"];
+            }
+            Frontends.update(frontend._id, { $set: newFields });
+          }
+        }
+      });
+    }
+  }
 ), 2 * 3600 * 1000); // every two hours
 
 
